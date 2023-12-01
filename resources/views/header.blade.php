c1488a870ca0b90ffab27167e37094a429555a85
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2023-12-01 16:18:05 -0300

    headerAnimations

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -37,17 +37,17 @@
     </svg></button>
               <!-- Dropdown menu -->
               <div id='dropdownNavbar' class='anchorStyle z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600'>
-                  <ul class='py-2 text-sm text-gray-700 dark:text-gray-400' aria-labelledby='dropdownLargeButton'>
+                  <ul class='dropdownActiveStyle py-2 text-sm text-gray-700 dark:text-gray-400' aria-labelledby='dropdownLargeButton'>
                     <li>
-                      <a href='/dashboard' class='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>Dashboard</a>
+                      <a href='/dashboard' class='linkStyleHeader block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>Dashboard</a>
                     </li>
                     <li>
-                      <a href='#' class='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>Game</a>
+                      <a href='#' class='linkStyleHeader block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>Game</a>
                     </li>
                   
                   </ul>
                   <div class='py-1'>
-                    <a href='/' class='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white'>Sign out</a>
+                    <a href='/' class='linkStyleHeader block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white'>Sign out</a>
                   </div>
               </div>
           </li>
